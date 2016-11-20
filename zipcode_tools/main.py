10ca1c3682b61931ea2a496008035b64d93b3d6f
laurenwolfe <lulumialu@gmail.com>
2016-11-19 16:11:37 -0800

    testing

--- zipcode_tools/main.py
+++ zipcode_tools/main.py
@@ -1,4 +1,5 @@
-# from database import *
+#!/usr/bin/python
+import sys
 from find_zipcodes import *
 
 """Creating a new database instance and inserting data, then confirming count."""
@@ -13,4 +14,13 @@ from find_zipcodes import *
 FindZipcodes' constructor takes two arguments: zipcode and distance (in miles).
 Calling get_zipcodes() on the instance will return a list of all matching zipcodes whose centers fall within the region.
 """
-print FindZipcodes(98107, 3).get_zipcodes()
+
+
+def main(argv):
+    if len(argv) < 3:
+        print "usage: zipcode, distance (mi)"
+
+    zipcode = int(argv[1])
+    distance = int(argv[2])
+
+    print FindZipcodes(zipcode, distance).get_zipcodes()
